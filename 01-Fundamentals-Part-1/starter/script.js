9c1b0c8d8829b7e40960842f0afb1005a735bc4a
dlanger96 <67539028+MrN3XU5@users.noreply.github.com>
2021-03-01 09:35:04 +0100

    Course
    JavaScript tutorial

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,52 @@
+/*
+let js = "Amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+let first = "Marko";
+let newFirstName = "Srecko";
+let first_name = "Stevan";
+console.log(firstName);
+console.log(first);
+console.log(newFirstName);
+// tipovi
+let isIsland = false;
+let population = 4;
+let countryName = "Croatia";
+
+console.log(isIsland);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof countryName);
+console.log(`"Ime zemlje je ${countryName}, a broj stanovnika iznosi ${population}"`);
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Dominik");
+
+javascriptIsFun = "YES!";
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+*/
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+
+var job = "Programm er";
+console.log(`Godina rođenja ${typeof birthYear}  Posoa ${typeof job}`);
