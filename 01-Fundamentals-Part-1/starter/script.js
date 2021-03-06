b840497964bd0885bd7a1dc2bd7306d341bd32a0
dominik <dominikl520@gmail.com>
2021-03-06 12:39:45 +0100

    The Conditional (Ternary) operator

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -275,7 +275,7 @@ else {
     console.log("NO Winer");
 }
 
-*/
+
 
 const day = "thursday";
 
@@ -303,3 +303,22 @@ switch (day) {
         console.log("Not a valid day!");
         break;
 }
+
+*/
+
+const age = 25;
+age >= 18 ? console.log("I like to drink wine ") : console.log("I like to drink Water");
+
+const drink = age >= 18 ? "wine" : "water" ;
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+    drink2 = "wine";
+}
+else {
+    drink2 = "water";
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
