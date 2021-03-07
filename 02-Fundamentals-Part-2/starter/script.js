56ba642493130ce2fdf3f1c24bd75999e47d2f9a
dlanger96 <67539028+dlanger96@users.noreply.github.com>
2021-03-07 18:17:42 +0100

    033 Funkcije

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -9,7 +9,7 @@ if (passtest) {
 
 hasDriversLicence == true ? console.log("I can drive!!") : console.log("STOP!!");
 
-*/
+
 // 033 Funkcije
 function logger(){
     console.log("My name is Dominik");
@@ -28,3 +28,20 @@ console.log(appleJuic);
 
 const appleOrangeJuice = fruitProcessor(2,4);
 console.log(appleOrangeJuice);
+*/
+
+function calcAge1(birthYear) {
+    //const currentYear = Number(new Date().getFullYear());
+    return 2037 - birthYear;
+}
+
+console.log(calcAge1(1996));
+
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const age2 = calcAge2(1996);
+
+console.log(age2)
+
